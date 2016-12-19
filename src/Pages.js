0b133afc6a1f0c68368b2e9bfb4e1be6b008443b
SATOSHI NAKAJIMA <satoshi.nakajima@gmail.com>
2016-12-19 15:09:07 -0800

    subToolbar

--- src/Pages.js
+++ src/Pages.js
@@ -8,11 +8,17 @@ class Pages extends Component {
       <div className='canvasPages'
            style={{height:this.props.height - height}}>{
         this.props.pages.map((page, index) => {
-           return <Page key={index} pageIndex={index} page={page}
-               width={ this.props.width }
-               dimension={ this.props.dimension }
-               sceneElements={ this.props.sceneElements }
-               />
+           return (<div key={index}>
+                <Page pageIndex={index} page={page}
+                  width={ this.props.width }
+                  dimension={ this.props.dimension }
+                  sceneElements={ this.props.sceneElements }
+                />
+                <div className="subToolbar">
+                  <button onClick={()=>{window.store.dispatch({type:'deletePage', pageIndex:index})}} >Delete</button>
+                  <button onClick={()=>{window.store.dispatch({type:'duplicatePage', page:page, pageIndex:index})}} >Duplicate</button>
+                </div>
+            </div>)
         })
       }</div>
     )
