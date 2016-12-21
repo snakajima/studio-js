c098c4bbd1778e5aec1895e2905cc30d21d1ed86
SATOSHI NAKAJIMA <satoshi.nakajima@gmail.com>
2016-12-21 12:01:29 -0800

    apply and delta

--- src/Generator.js
+++ src/Generator.js
@@ -4,9 +4,29 @@
 //
 
 function generate(store) {
+  function applyTransform(elements, deltas) {
+    return elements.map((element)=>{
+                        var e = Object.assign({}, element);
+                        const delta = deltas[element.id];
+                        if (delta) {
+                          console.log("delta=", JSON.stringify(delta));
+                          if (delta.translate) {
+                            e.x += delta.translate[0];
+                            e.y += delta.translate[1];
+                          }
+                        }
+                        return e;
+                      });
+  }
+  function delta(base, element) {
+    //if (base.x != element.x || base.y != element.y) {
+    return {id:base.id, translate:[element.x-base.x, element.y-base.y]};
+    //}
+  }
+  
     const state = store.getState();
-    var prev = {};
-    const idsAll = state.elements.map((element) => element.id);
+    var prev = state.elements;
+    //const idsAll = state.elements.map((element) => element.id);
     return {
       type:"net.swipe.swipe",
       dimension:[state.dimension.width, state.dimension.height],
@@ -22,14 +42,15 @@ function generate(store) {
       },
       pages: state.pages.map((page) => {
         var obj = {template:"s0"}
-        const ids = idsAll.filter((id) => (prev[id] || page[id]));
-        if (ids.length > 0) {
-          obj.elements = ids.map((id) => {
-            let element = Object.assign({id:id}, prev[id] || {});
-            return Object.assign(element, {to:page[id] || {translate:[0,0]}});
-          })
-        };
-        prev = page;
+        var elements = applyTransform(prev, page);
+        console.log("elements=", JSON.stringify(elements));
+        obj.elements = elements.reduce((s, element, index) => {
+                                       var d0 = delta(state.elements[index], prev[index]);
+                                       const d1 = delta(prev[index], element);
+                                       s.push(Object.assign(d0, {to:d1}));
+                                      return s;
+                                      }, []);
+        prev = elements;
         return obj;
       })
     };
