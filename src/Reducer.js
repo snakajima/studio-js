563cf38e71da01db99c2cff54c6491cc34e265c6
SATOSHI NAKAJIMA <satoshi.nakajima@gmail.com>
2016-12-21 10:56:16 -0800

    elementMap

--- src/Reducer.js
+++ src/Reducer.js
@@ -6,8 +6,17 @@
 import MathEx from './MathEx';
 
 function reducer(_state, action) {
-    if (typeof _state === "undefined") {
-      const initialState = {
+  // The elementMap maps the element id to index.
+  // We store it as a property of the state only for convenience.
+  function elementMap(elements) {
+    return elements.reduce((map, v, index)=>{
+                            map[v.id] = index;
+                            return map;
+                           }, {});
+  }
+  
+  if (typeof _state === "undefined") {
+    var initialState = {
         selection:new Set(),
         pageIndex:0,
         preview:false,
@@ -20,84 +29,86 @@ function reducer(_state, action) {
             id:"i2", x:80, y:50, h:30, w:50, bc:'#00ff00', "img":"http://satoshi.blogs.com/swipe/shuttlex.png"
           }],
         pages:[{}]
-      };
-      window.stack.append(initialState);
-      return initialState;
-    }
-    // *** IMPORTANT ***
-    // In order to enable undo and redo, we must strictly follow the Redux guideline.
-    // Do not modify the state object or its sub-objects (no side effect).
-    var state = Object.assign({}, _state);
-    var undoable = true;
-    switch(action.type) {
-        case 'duplicateScene':
-            state.pages = state.pages.map((page) => page);
-            state.pages.unshift({});
-            break;
-        case 'duplicatePage':
-            state.pages = state.pages.map((page) => page);
-            state.pages.splice(action.pageIndex + 1, 0, Object.assign({}, action.page));
-            break;
-        case 'deletePage':
-            state.pages = state.pages.map((page) => page);
-            state.pages.splice(action.pageIndex, 1);
-            if (state.pageIndex >=state.pages.length) {
-                state.pageIndex = state.pages.length-1;
+    };
+    initialState.elementMap = elementMap(initialState.elements);
+    window.stack.append(initialState);
+    return initialState;
+  }
+  // *** IMPORTANT ***
+  // In order to enable undo and redo, we must strictly follow the Redux guideline.
+  // Do not modify the state object or its sub-objects (no side effect).
+  var state = Object.assign({}, _state);
+  var undoable = true;
+  switch(action.type) {
+    case 'duplicateScene':
+        state.pages = state.pages.map((page) => page);
+        state.pages.unshift({});
+        break;
+    case 'duplicatePage':
+        state.pages = state.pages.map((page) => page);
+        state.pages.splice(action.pageIndex + 1, 0, Object.assign({}, action.page));
+        break;
+    case 'deletePage':
+        state.pages = state.pages.map((page) => page);
+        state.pages.splice(action.pageIndex, 1);
+        if (state.pageIndex >=state.pages.length) {
+            state.pageIndex = state.pages.length-1;
+        }
+        break;
+    case 'moveSceneElement':
+        console.log("moveSceneElement", action.handle);
+        state.elements = state.elements.map((element)=>{
+            if (element.id === action.id) {
+              var e = Object.assign({}, element);
+              e.x = MathEx.round(e.x + action.dx / action.scale);
+              e.y = MathEx.round(e.y + action.dy / action.scale);
+              return e;
             }
-            break;
-        case 'moveSceneElement':
-            console.log("moveSceneElement", action.handle);
-            state.elements = state.elements.map((element)=>{
-                if (element.id === action.id) {
-                  var e = Object.assign({}, element);
-                  e.x = MathEx.round(e.x + action.dx / action.scale);
-                  e.y = MathEx.round(e.y + action.dy / action.scale);
-                  return e;
-                }
-                return element
-            })
-            break;
-        case 'movePageElement':
-            console.log("movePageElement", action.handle);
-            var page = Object.assign({}, state.pages[action.pageIndex]);
-            var element = Object.assign({}, page[action.id] || {});
-            const tx = element.translate || [0,0];
-            element.translate = [
-              MathEx.round(tx[0] + action.dx / action.scale),
-              MathEx.round(tx[1] + action.dy / action.scale)
-            ];
-            page[action.id] = element;
-            state.pages = state.pages.map((page) => page);
-            state.pages[action.pageIndex] = page;
-            break;
-        case 'resize':
-            undoable = false;
-            break;
-        case 'preview':
-            state.preview = action.preview;
-            undoable = false;
-            break;
-        case 'selectPage':
-            state.pageIndex = action.pageIndex;
-            undoable = false;
-            break;
-        case 'setState':
-            state = action.state;
-            undoable = false;
-            break;
-        case 'selectElement':
-            state.selection = action.selection;
-            undoable = false;
-            break;
-        default:
-            console.log("unknown type:" + action.type);
-            undoable = false;
-            break;
-    }
-    if (undoable) {
-        window.stack.append(state);
-    }
-    return state
+            return element
+        })
+        break;
+    case 'movePageElement':
+        console.log("movePageElement", action.handle);
+        var page = Object.assign({}, state.pages[action.pageIndex]);
+        var element = Object.assign({}, page[action.id] || {});
+        const tx = element.translate || [0,0];
+        element.translate = [
+          MathEx.round(tx[0] + action.dx / action.scale),
+          MathEx.round(tx[1] + action.dy / action.scale)
+        ];
+        page[action.id] = element;
+        state.pages = state.pages.map((page) => page);
+        state.pages[action.pageIndex] = page;
+        break;
+    case 'resize':
+        undoable = false;
+        break;
+    case 'preview':
+        state.preview = action.preview;
+        undoable = false;
+        break;
+    case 'selectPage':
+        state.pageIndex = action.pageIndex;
+        undoable = false;
+        break;
+    case 'setState':
+        state = action.state;
+        undoable = false;
+        break;
+    case 'selectElement':
+        state.selection = action.selection;
+        undoable = false;
+        break;
+    default:
+        console.log("unknown type:" + action.type);
+        undoable = false;
+        break;
+  }
+  if (undoable) {
+    window.stack.append(state);
+  }
+  state.elementMap = elementMap(state.elements);
+  return state
 }
 
 module.exports = { reducer };
