a1ad87516d1f4c565f58365881df8053a350a0dd
Satoshi Nakajima <satoshi@uievolution.com>
2016-12-21 22:02:14 -0800

    fixed translate bug

--- src/Generator.js
+++ src/Generator.js
@@ -2,26 +2,29 @@
 // Copyright (c) 2016 Satoshi Nakajima (https://github.com/snakajima)
 // License: The MIT License
 //
+import MathEx from './MathEx';
 
 function generate(store) {
   function applyTransform(elements, deltas) {
     return elements.map((element)=>{
-                        var e = Object.assign({}, element);
-                        const delta = deltas[element.id];
-                        if (delta) {
-                          console.log("delta=", JSON.stringify(delta));
-                          if (delta.translate) {
-                            e.x += delta.translate[0];
-                            e.y += delta.translate[1];
-                          }
-                        }
-                        return e;
-                      });
+        var e = Object.assign({}, element);
+        const delta = deltas[element.id];
+        if (delta) {
+          console.log("delta=", JSON.stringify(delta));
+          if (delta.translate) {
+            e.x += delta.translate[0];
+            e.y += delta.translate[1];
+          }
+        }
+        return e;
+      });
   }
   function delta(base, element) {
-    //if (base.x != element.x || base.y != element.y) {
-    return {id:base.id, translate:[element.x-base.x, element.y-base.y]};
-    //}
+    if (base.x !== element.x || base.y !== element.y) {
+      return {translate:[MathEx.round(element.x-base.x),
+                         MathEx.round(element.y-base.y)]};
+    }
+    return null;
   }
   
     const state = store.getState();
@@ -42,14 +45,23 @@ function generate(store) {
       },
       pages: state.pages.map((page) => {
         var obj = {template:"s0"}
-        var elements = applyTransform(prev, page);
+        var elements = applyTransform(state.elements, page);
         console.log("elements=", JSON.stringify(elements));
         obj.elements = elements.reduce((s, element, index) => {
-                                       var d0 = delta(state.elements[index], prev[index]);
-                                       const d1 = delta(prev[index], element);
-                                       s.push(Object.assign(d0, {to:d1}));
-                                      return s;
-                                      }, []);
+               var d0 = delta(state.elements[index], prev[index]);
+               const d1 = delta(state.elements[index], element);
+               if (d0) {
+                  d0.id = element.id;
+                  if (d1) {
+                    s.push(Object.assign(d0, {to:d1}));
+                  } else {
+                    s.push(Object.assign(d0, {to:{translate:[0,0]}}));
+                  }
+               } else if (d1) {
+                 s.push({id:element.id, to:d1});
+               }
+              return s;
+              }, []);
         prev = elements;
         return obj;
       })
--- src/MathEx.js
+++ src/MathEx.js
@@ -5,7 +5,7 @@
 
 class MathEx {
     static round(value, precision=10) {
-        return Math.floor(value * precision) / precision;
+        return Math.floor(value * precision + precision/2) / precision;
     }
 }
 
