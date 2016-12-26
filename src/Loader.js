07fa114f5eece9cb6e8f3210da609138748b5d49
Satoshi Nakajima <satoshi@uievolution.com>
2016-12-26 06:23:14 -0800

    load with rotate and scale

--- src/Loader.js
+++ src/Loader.js
@@ -6,10 +6,17 @@
 function load(swipe, prevState) {
   const str = JSON.stringify(swipe, undefined, 2);
   console.log(str);
+  const sceneElements = swipe.templates.pages.s0.elements;
+  const elementMap = sceneElements.reduce((map, element)=>{
+    map[element.id] = element;
+    return map;
+  }, {});
   const pages = swipe.pages.map((page) => {
     if (page.elements) {
       return page.elements.reduce((ret, element)=>{
         var value = {};
+        const sceneElement = elementMap[element.id];
+        console.log("load.map", sceneElement);
         if (element.to) {
           console.log('load:element=', JSON.stringify(element.to));
           if (element.to.translate) {
@@ -20,9 +27,16 @@ function load(swipe, prevState) {
           }
           if (element.to.rotate) {
             value.rotate = element.to.rotate;
+            if (sceneElement.rotate) {
+              value.rotate -= sceneElement.rotate;
+            }
           }
           if (element.to.scale) {
             value.scale = element.to.scale;
+            if (sceneElement.scale) {
+              value.scale[0] /= sceneElement.scale[0];
+              value.scale[1] /= sceneElement.scale[1];
+            }
           }
         }
         ret[element.id] = value;
@@ -36,7 +50,7 @@ function load(swipe, prevState) {
     pageIndex:0,
     preview:false,
     dimension:{ width:swipe.dimension[0] || 320, height:swipe.dimension[1] || 320 },
-    elements:swipe.templates.pages.s0.elements,
+    elements:sceneElements,
     pages:pages
   };
   return state;
