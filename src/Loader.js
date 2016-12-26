f3e8a267f74408416393a87fa59ec68f43786b92
Satoshi Nakajima <satoshi@uievolution.com>
2016-12-26 05:30:38 -0800

    load:pages

--- src/Loader.js
+++ src/Loader.js
@@ -6,13 +6,16 @@
 function load(swipe, prevState) {
   const str = JSON.stringify(swipe, undefined, 2);
   console.log(str);
+  const pages = swipe.pages.map((page) => {
+    return {}
+  });
   var state = {
     selection:{ids:new Set()},
     pageIndex:0,
     preview:false,
     dimension:{ width:swipe.dimension[0] || 320, height:swipe.dimension[1] || 320 },
     elements:swipe.templates.pages.s0.elements,
-    pages:[{}]
+    pages:pages
   };
   return state;
 }
