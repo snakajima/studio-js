5b4d91c131e94cb7f9331aef9092cf21b4646897
Satoshi Nakajima <satoshi@uievolution.com>
2016-12-26 05:56:04 -0800

    load:properties

--- src/Loader.js
+++ src/Loader.js
@@ -7,6 +7,28 @@ function load(swipe, prevState) {
   const str = JSON.stringify(swipe, undefined, 2);
   console.log(str);
   const pages = swipe.pages.map((page) => {
+    if (page.elements) {
+      return page.elements.reduce((ret, element)=>{
+        var value = {};
+        if (element.to) {
+          console.log('load:element=', JSON.stringify(element.to));
+          if (element.to.translate) {
+            value.translate = element.to.translate;
+          }
+          if (element.to.opacity) {
+            value.opacity = element.to.opacity;
+          }
+          if (element.to.rotate) {
+            value.rotate = element.to.rotate;
+          }
+          if (element.to.scale) {
+            value.scale = element.to.scale;
+          }
+        }
+        ret[element.id] = value;
+        return ret;
+      }, {});
+    }
     return {}
   });
   var state = {
