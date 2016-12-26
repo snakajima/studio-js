c5a19a2c92b0450c8698c09f800d934831f7ddb6
Satoshi Nakajima <satoshi@uievolution.com>
2016-12-26 05:27:01 -0800

    Loader

--- src/Loader.js
+++ src/Loader.js
@@ -3,9 +3,17 @@
 // License: The MIT License
 //
 
-function load(swipe, state) {
+function load(swipe, prevState) {
   const str = JSON.stringify(swipe, undefined, 2);
   console.log(str);
+  var state = {
+    selection:{ids:new Set()},
+    pageIndex:0,
+    preview:false,
+    dimension:{ width:swipe.dimension[0] || 320, height:swipe.dimension[1] || 320 },
+    elements:swipe.templates.pages.s0.elements,
+    pages:[{}]
+  };
   return state;
 }
 
